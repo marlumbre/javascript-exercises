11b26742ccf8686ad2dc1b84309dea6a9d7638e3
marlumbre <101400268+marlumbre@users.noreply.github.com>
2025-04-21 22:15:46 +0800

    Finish 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let splitString = str.split("");
+    let reverseArray = splitString.reverse();
+    let reverseString = reverseArray.join("");
+    return reverseString;
 };
 
 // Do not edit below this line
