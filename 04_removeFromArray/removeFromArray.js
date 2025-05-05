fef9f540a8263f4e16a1b21f3f76dd1901018f36
marlumbre <101400268+marlumbre@users.noreply.github.com>
2025-05-05 15:35:51 +0800

    Attempt 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(givenArray, toRemove) {
+    while(true) {
+        let indexToRemove = givenArray.indexOf(toRemove);
+        givenArray.splice(indexToRemove, 1);
+
+        return givenArray;
+    }
 };
 
 // Do not edit below this line
