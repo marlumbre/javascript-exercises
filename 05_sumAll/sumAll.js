c4a68cb29892417760917f0aa14e852997ad862d
marlumbre <101400268+marlumbre@users.noreply.github.com>
2025-05-05 15:09:17 +0800

    Finish 05_addAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,10 +1,25 @@
 const sumAll = function(firstInteger, secondInteger) {
 
-    for ( i = firstInteger; i < secondInteger; i++ ) {
+if ( Number.isInteger(firstInteger) !== true || Number.isInteger(secondInteger) !== true ) {
+    return "ERROR";
+}
+
+if ( firstInteger > secondInteger ) { 
+    [firstInteger, secondInteger] = [secondInteger, firstInteger]
+}
+
+if ( firstInteger < 0 || secondInteger < 0 ) {
+    return "ERROR";
+    }
+
+    let sum = 0;
+
+    for ( i = firstInteger; i <= secondInteger; i++ ) {
         sum += i;
     }
 
     return sum;
+    
 
 };
 
