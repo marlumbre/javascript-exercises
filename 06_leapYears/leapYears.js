0573ff3ec28fd1c2aaad8bd341d95c8a50bb076f
marlumbre <101400268+marlumbre@users.noreply.github.com>
2025-05-10 17:05:04 +0800

    Finish 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const isYearDivisibleByFour = year % 4 === 0;
+    const isYearCentury = year % 100 === 0;
+    const isYearDivisibleByFourHundred = year % 400 === 0;
 
+    if ( isYearDivisibleByFour && (!isYearCentury || isYearDivisibleByFourHundred )) {
+        return true;
+    } else return false;
 };
 
 // Do not edit below this line
